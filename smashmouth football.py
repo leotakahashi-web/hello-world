74ed25453f3a8b3b70681ea1476508fe2c748b6f
leotakahashi-web <leonardogalocura13@gmail.com>
2025-10-26 21:24:15 -0400

    smashmouth_football

---
+++ smashmouth football.py
@@ -0,0 +1,40 @@
+# Smashmouth Football Program
+# Demonstrates Object-Oriented Programming with classes for NFL teams and players
+
+# Define Player class
+class Player:
+    def __init__(self, playerName, playerPosition):
+        self.playerName = playerName
+        self.playerPosition = playerPosition
+
+    def __str__(self):
+        return f"{self.playerName} - {self.playerPosition}"
+
+
+# Define NFLTeam class
+class NFLTeam:
+    def __init__(self, teamName, players):
+        self.teamName = teamName
+        self.players = players  # list of Player objects
+
+    def display_team(self):
+        print(f"Team Name: {self.teamName}")
+        print("Players:")
+        for player in self.players:
+            print(f"  {player}")
+
+
+# Create players
+player1 = Player("Joe Montana", "QB")
+player2 = Player("Barry Sanders", "RB")
+player3 = Player("Jerry Rice", "WR")
+player4 = Player("Graham Gano", "K")
+
+# Add players to list
+playerList = [player1, player2, player3, player4]
+
+# Create team
+team = NFLTeam("Smashmouth Warriors", playerList)
+
+# Display team info
+team.display_team()
